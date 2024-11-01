907ecfd3011702237747efb5387b51cf7c640d02
martinbouhier <martinbouhier@outlook.com>
2024-10-31 22:51:21 -0300

    add all services icons

---
+++ app/src/main/java/com/example/parcialtp3langmanpoltibohuier/ui/components/icons/services/prestamos.kt
@@ -0,0 +1,11 @@
+package com.example.parcialtp3langmanpoltibohuier.ui.components.icons.services
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.example.parcialtp3langmanpoltibohuier.R
+
+@Composable
+fun getIconPrestamos(): Painter {
+    return painterResource(id = R.drawable.ic_prestamos)
+}
